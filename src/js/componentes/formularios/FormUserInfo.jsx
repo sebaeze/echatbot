4238de15ba24b31880c2f62c60e64e53744af661
andreole <andreole@ar.ibm.com>
2019-10-25 15:37:06 -0300

    form account user v2

--- src/js/componentes/formularios/FormUserInfo.jsx
+++ src/js/componentes/formularios/FormUserInfo.jsx
@@ -4,7 +4,8 @@
 /*
 *
 */
-import React                                                    from 'react' ;
+import React                   from 'react' ;
+import { FiltroPais }          from '../filtros/FiltroPais' ;
 import { Row, Col, Spin, Form, Input, Button, Tooltip, Icon, Modal }   from 'antd'  ;
 //
 class FormUserInfo extends React.Component {
@@ -21,14 +22,11 @@ class FormUserInfo extends React.Component {
     onSubmitConsulta(argEventCont){
         try {
             argEventCont.preventDefault() ;
-            //
-            console.log('....onSubmitConsulta:: ') ;
             let arrayInputForm = argEventCont.target.querySelectorAll('input,textarea') ;
             let objForm        = {} ;
             arrayInputForm.forEach((elemInp)=>{
                 objForm[ elemInp.id ] = elemInp.value || elemInp.textContent || elemInp.innerHTML || false ;
             }) ;
-            console.dir(objForm) ;
             //
         } catch(argEventCont){
             console.dir(argEventCont) ;
@@ -102,83 +100,139 @@ class FormUserInfo extends React.Component {
                         <span style={{fontSize:'32px'}} >Perfecto!</span>
                     </Modal>
                     <Form onSubmit={onSubmit} style={ {...estiloForm} } >
-                    <Form.Item
-                        label={ <span>{this.props.translate.form.name}<Tooltip  placement="bottomRight" title="¿ Cuál es su nombre o cómo le gusta que lo llamen ?"> <Icon type="question-circle-o" /> </Tooltip> </span> }
-                    >
-                        {getFieldDecorator('name', { rules: [{ required: true, message: 'Por favor, escriba su nombre', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item
-                        label={ <span>{this.props.translate.form.lastName}</span> }
-                    >
-                        {getFieldDecorator('lastName', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.address}</span> } >
-                        {getFieldDecorator('address', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.city}</span> } >
-                        {getFieldDecorator('city', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.state}</span> } >
-                        {getFieldDecorator('state', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.zipCode}</span> } >
-                        {getFieldDecorator('zipCode', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.country}</span> } >
-                        {getFieldDecorator('pais', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item label="E-mail">
-                        {getFieldDecorator('email', {
-                            rules: [
-                            {
-                                type: 'email',
-                                message: 'Email incorrecto.',
-                            },
-                            {
-                                required: true,
-                                message: 'Por favor, ponga un email correcto.',
-                            },
-                            ],
-                        }
-                        )(<Input />)}
-                    </Form.Item>
-                    <Form.Item
-                        label={
-                            <span>{this.props.translate.form.phone}:
-                            <Tooltip  placement="bottomRight" title="Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ">
-                                <Icon type="question-circle-o" />
-                            </Tooltip>
-                            </span>
-                        }
-                    >
-                    {getFieldDecorator('phone', {
-                        rules: [{ required: false, message: 'Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ' }],
-                    })(<Input style={{ width: '100%' }} />)}
-                    </Form.Item>
-                    <Form.Item
-                        label={<span>Whatsapp:
-                            <Tooltip  placement="bottomRight" title="Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ">
-                                <Icon type="question-circle-o" />
-                            </Tooltip>
-                            </span>
-                        }
-                    >
-                        {getFieldDecorator('whatsapp', { rules: [{ required: false, message: 'Ingrese su número de Whatsapp si desea recibir respuesta por Whatsapp' }], })
-                        (<Input style={{ width: '100%' }} />)}
-                    </Form.Item>
-                    <Form.Item  label={ <span>{this.props.translate.form.description}</span> } >
-                        {getFieldDecorator('description', { rules: [{ required: false, message: 'Comentarios adicionales', whitespace: true }], })(<Input />)}
-                    </Form.Item>
-                    <Form.Item>
-                        {
-                            this.state.flagSpinner==true ?
-                                <Spin size="large" />
-                                :
-                                <Button type="primary" size={"large"} htmlType="submit" style={{padding:'0',margin:'0',width:'200px'}} >
-                                    {this.props.translate.form.savechanges}
-                                </Button>
-                        }
-                    </Form.Item>
+                        <Row style={{marginTop:'20px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item
+                                    label={ <span>{this.props.translate.form.name}<Tooltip  placement="bottomRight" title="¿ Cuál es su nombre o cómo le gusta que lo llamen ?"> <Icon type="question-circle-o" /> </Tooltip> </span> }
+                                >
+                                    {getFieldDecorator('name', { rules: [{ required: true, message: 'Por favor, escriba su nombre', whitespace: true }], })(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item
+                                    label={ <span>{this.props.translate.form.lastName}</span> }
+                                >
+                                    {getFieldDecorator('lastName', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={21} xl={21} xxl={21}>
+                                <Form.Item label="E-mail">
+                                    {getFieldDecorator('email', {
+                                        rules: [
+                                        {
+                                            type: 'email',
+                                            message: 'Email incorrecto.',
+                                        },
+                                        {
+                                            required: true,
+                                            message: 'Por favor, ponga un email correcto.',
+                                        },
+                                        ],
+                                    }
+                                    )(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={21} xl={21} xxl={21}>
+                                <Form.Item  label={ <span>{this.props.translate.form.address}</span> } >
+                                    {getFieldDecorator('address', {})(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item  label={ <span>{this.props.translate.form.city}</span> } >
+                                    {getFieldDecorator('city', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item  label={ <span>{this.props.translate.form.state}</span> } >
+                                    {getFieldDecorator('state', { rules: [{ required: true, message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item  label={ <span className="ant-form-item-required">{this.props.translate.form.country}</span> } >
+                                    <FiltroPais  id={"idFiltroPais"} style={{width:'100%'}}
+                                                seleccion={(argSelPais)=>{
+                                                    console.log('....') ;
+                                                    console.dir(this.state.userInfo) ;
+                                                    let tempForm = this.state.userInfo ;
+                                                    tempForm.pais = argSelPais ;
+                                                    this.setState({userInfo:tempForm})
+                                                }}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item  label={ <span>{this.props.translate.form.zipCode}</span> } >
+                                    {getFieldDecorator('zipCode', { rules: [{ message: 'Por favor, escriba su apellido', whitespace: true }], })(<Input size="large" />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item
+                                    label={
+                                        <span>{this.props.translate.form.phone}:
+                                        <Tooltip  placement="bottomRight" title="Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ">
+                                            <Icon type="question-circle-o" />
+                                        </Tooltip>
+                                        </span>
+                                    }
+                                >
+                                {getFieldDecorator('phone', {
+                                    rules: [{ required: false, message: 'Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ' }],
+                                })(<Input size="large" style={{ width: '100%' }} />)}
+                                </Form.Item>
+                            </Col>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={10} xl={10} xxl={10}>
+                                <Form.Item
+                                    label={<span>Whatsapp:
+                                        <Tooltip  placement="bottomRight" title="Ingrese su número de Whatsapp / teléfono si desea recibir respuesta por Whatsapp / Teléfono ">
+                                            <Icon type="question-circle-o" />
+                                        </Tooltip>
+                                        </span>
+                                    }
+                                >
+                                    {getFieldDecorator('whatsapp', { rules: [{ required: false, message: 'Ingrese su número de Whatsapp si desea recibir respuesta por Whatsapp' }], })
+                                    (<Input size="large" style={{ width: '100%' }} />)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row style={{marginTop:'5px'}}>
+                            <Col xs={1} md={1} lg={1} xl={1} xxl={1}></Col>
+                            <Col xs={23} md={23} lg={21} xl={21} xxl={21}>
+                                <Form.Item  label={ <span>{this.props.translate.form.description}</span> } >
+                                    {getFieldDecorator('description', { rules: [{ required: false, message: 'Comentarios adicionales', whitespace: true }], })
+                                    (<Input.TextArea />)}
+                                </Form.Item>
+                                <Form.Item>
+                                    {
+                                        this.state.flagSpinner==true ?
+                                            <Spin size="large" />
+                                            :
+                                            <Button type="primary" size={"large"} htmlType="submit" style={{padding:'0',margin:'0',width:'200px'}} >
+                                                {this.props.translate.form.savechanges}
+                                            </Button>
+                                    }
+                                </Form.Item>
+                            </Col>
+                        </Row>
                 </Form>
                 </Col>
             </Row>
